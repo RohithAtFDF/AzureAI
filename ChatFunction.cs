51204cd1bf1d1bb1241a6b06ea783d229d50b0ad
RohithAtFDF <rr0076@firstday.foundation>
2026-07-02 14:04:21 -0500

    trying a txt index rag

--- ChatFunction.cs
+++ ChatFunction.cs
@@ -17,7 +17,7 @@ public class ChatFunction
         try
         {
             var searchEndpoint = "https://cisaisearchservice.search.windows.net";
-            var indexName = "texas-driving-handbook";
+            var indexName = "texas-driver-manual-txt-format";
             var searchKey = Environment.GetEnvironmentVariable("SEARCH_KEY", EnvironmentVariableTarget.Process);
 
             if (string.IsNullOrEmpty(searchKey))
