b8a0ae7dbc25c2aba9a1d8f68dffde5afe505bcc
RohithAtFDF <rr0076@firstday.foundation>
2026-08-05 14:13:24 -0500

    refactor RecentQuestions function to use Azure Functions SDK and simplify response structure

--- RecentQuestions.cs
+++ RecentQuestions.cs
@@ -1,56 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Azure.Data.Tables;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
 
 public class RecentQuestions
 {
-    private readonly TableServiceClient _tableServiceClient;
-
-    public RecentQuestions(TableServiceClient tableServiceClient)
+    [Function("RecentQuestions")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get",
+        Route = "recent-questions")]
+        HttpRequestData req)
     {
-        _tableServiceClient = tableServiceClient;
-    }
+        var response = req.CreateResponse(HttpStatusCode.OK);
 
-    [FunctionName("RecentQuestions")]
-    public async Task<IActionResult> Run(
-        [HttpTrigger(
-            AuthorizationLevel.Anonymous,
-            "get",
-            Route = "recent-questions")]
-        HttpRequest req)
-    {
-        var user = AuthUserExtractor.GetUser(req);
-
-        var tableClient =
-            _tableServiceClient.GetTableClient("ChatFeedback");
-
-        var questions = new List<string>();
-
-        await foreach (var entity in tableClient.QueryAsync<TableEntity>())
+        await response.WriteAsJsonAsync(new
         {
-            if (
-                entity.TryGetValue("Email", out var emailObj) &&
-                emailObj?.ToString()
-                    ?.Equals(user.Email, StringComparison.OrdinalIgnoreCase) == true &&
-                entity.TryGetValue("Question", out var questionObj)
-            )
+            queries = new[]
             {
-                questions.Add(questionObj?.ToString() ?? "");
+                "Test Question 1",
+                "Test Question 2"
             }
-        }
-
-        var latest = questions
-            .Where(q => !string.IsNullOrWhiteSpace(q))
-            .Distinct()
-            .TakeLast(10)
-            .Reverse()
-            .ToList();
-
-        return new OkObjectResult(new
-        {
-            queries = latest
         });
+
+        return response;
     }
 }
